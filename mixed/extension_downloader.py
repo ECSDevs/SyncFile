6f1996b62c22acd81e1135ac850b0cb3c57a53de
lghilb-yu <2438926613@qq.com>
2023-04-29 00:05:49 +0800

    fix issue.

--- mixed/extension_downloader.py
+++ mixed/extension_downloader.py
@@ -1,3 +1,5 @@
 print("downloading extension wget")
 from down import downloadFile
+from os import rename
 downloadFile("https://ghproxy.com/https://github.com/PsBash-Team/MCSMT/releases/download/exts/wget_ext.exe")
+rename("wget_ext.exe","wget.exe")
