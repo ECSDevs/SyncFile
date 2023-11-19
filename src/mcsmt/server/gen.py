d332f50ffc33e7aeb2d439c98e751eabb233a748
originalFactor <2438926613@qq.com>
2023-11-19 16:57:36 +0800

    fix some logical wrong

--- src/mcsmt/server/gen.py
+++ src/mcsmt/server/gen.py
@@ -13,13 +13,14 @@ def do_job(config="config.json",client="index.json"):
     # New: allow reverse join
     cc = {}
     for obj in config:
-        if obj[2] not in cc:
-            cc[obj[2]] = []
         fx = walk(obj[0]) if obj[3] else [(obj[0], [], [_ for _ in listdir(obj[0])])]
         for root, _, files in fx:
+            root = root
+            if obj[2]+() not in cc:
+                cc[obj[2]] = []
             for file in files:
                 if file.split('.')[-1] in obj[1]:
-                    filename = root.replace('\\','/')+'/'+file
+                    filename = root.replace('\\','/')+'/'+file if root!='.' else file
                     with open(filename, 'rb') as f:
                         mhash = sha512(f.read()).hexdigest()
                     cc[obj[2]].append([filename, mhash])
